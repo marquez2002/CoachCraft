d187e2a4d68a1125e47596d75bc03ebb5d814830
Gonzalo Marquez <gmdetorres@gmail.com>
2024-09-12 17:46:37 +0200

    🚧 construction: construction of the play library in progress

--- lib/screens/board/recording_plays_screen.dart
+++ lib/screens/board/recording_plays_screen.dart
@@ -1,25 +1,6 @@
+import 'package:CoachCraft/widgets/plays/football_plays_list.dart';
+import 'package:CoachCraft/widgets/plays/upload_plays_form.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:file_picker/file_picker.dart';
-import 'dart:typed_data';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jugadas de Estrategia',
-      home: RecordingPlayScreen(),
-    );
-  }
-}
 
 class RecordingPlayScreen extends StatefulWidget {
   @override
@@ -27,77 +8,7 @@ class RecordingPlayScreen extends StatefulWidget {
 }
 
 class _RecordingPlayScreenState extends State<RecordingPlayScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  String? _selectedType; // Tipo de jugada: ataque o defensa
-  Uint8List? _videoBytes; // Almacena los bytes del video seleccionado
-
-  // Función para seleccionar un video
-  Future<void> _selectVideo() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.video,
-    );
-
-    if (result != null) {
-      _videoBytes = result.files.first.bytes;
-      setState(() {});
-    }
-  }
-
-  // Función para subir el video
-  Future<String?> _uploadVideo() async {
-    if (_videoBytes != null) {
-      String fileName = '${DateTime.now().millisecondsSinceEpoch}.mp4';
-      Reference storageRef = FirebaseStorage.instance.ref().child('football_plays/$fileName');
-
-      try {
-        UploadTask uploadTask = storageRef.putData(_videoBytes!);
-        TaskSnapshot snapshot = await uploadTask;
-
-        if (snapshot.state == TaskState.success) {
-          String downloadUrl = await snapshot.ref.getDownloadURL();
-          return downloadUrl;
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Error al subir el video')),
-          );
-          return null;
-        }
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Error al subir el video: $e')),
-        );
-        return null;
-      }
-    }
-    return null; // Si no hay video, retornar null
-  }
-
-  Future<void> _handleSubmit() async {
-    if (_formKey.currentState!.validate()) {
-      String? videoUrl = await _uploadVideo();
-      if (videoUrl != null) {
-        await FirebaseFirestore.instance.collection('football_plays').add({
-          'name': _nameController.text.trim(),
-          'description': _descriptionController.text.trim(),
-          'type': _selectedType,
-          'videoUrl': videoUrl,
-        });
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Jugada guardada con éxito')),
-        );
-
-        // Limpiar los campos después de guardar
-        _nameController.clear();
-        _descriptionController.clear();
-        setState(() {
-          _videoBytes = null; // Limpiar los bytes del video
-          _selectedType = null; // Limpiar tipo de jugada
-        });
-      }
-    }
-  }
+  bool _isExpanded = false; // Controla el estado de expansión
 
   @override
   Widget build(BuildContext context) {
@@ -109,73 +20,25 @@ class _RecordingPlayScreenState extends State<RecordingPlayScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            ExpansionTile(
-              title: const Text('Subida de Jugadas'),
-              children: [
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _nameController,
-                        decoration: InputDecoration(labelText: 'Nombre de la jugada'),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor, ingresa un nombre';
-                          }
-                          return null;
-                        },
-                      ),
-                      DropdownButtonFormField<String>(
-                        value: _selectedType,
-                        hint: const Text('Seleccionar tipo'),
-                        items: [
-                          DropdownMenuItem(value: 'ataque', child: Text('Ataque')),
-                          DropdownMenuItem(value: 'defensa', child: Text('Defensa')),
-                        ],
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedType = value; // Actualiza el tipo seleccionado
-                          });
-                        },
-                        validator: (value) {
-                          if (value == null) {
-                            return 'Por favor, selecciona un tipo';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: _descriptionController,
-                        decoration: InputDecoration(labelText: 'Descripción (opcional)'),
-                      ),
-                      SizedBox(height: 20),
-                      // Botón para seleccionar video
-                      Container(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: _selectVideo,
-                          child: Text(
-                            _videoBytes != null ? 'Video ya seleccionado' : 'Seleccionar Video',
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20), // Espaciado entre botones
-                      // Botón para subir jugada
-                      Container(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: _handleSubmit,
-                          child: const Text('Subir Jugada'),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                          Text(' ')
-                    ],
-                  ),
-                ),
-              ],
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isExpanded = !_isExpanded; // Cambia el estado de expansión
+                });
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Subida de Jugadas', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Icon(_isExpanded ? Icons.expand_less : Icons.expand_more), // Ícono de expansión
+                ],
+              ),
             ),
+            const SizedBox(height: 8.0),
+            if (_isExpanded) // Solo muestra el formulario si está expandido
+              UploadForm(),
+            const SizedBox(height: 16.0),
+            Expanded(child: VideoList()), // Llama a VideoList aquí
           ],
         ),
       ),
---
+++ lib/widgets/plays/football_plays_list.dart
@@ -0,0 +1,147 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+class VideoList extends StatelessWidget {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  String getBackgroundImage(String type) {
+    switch (type) {
+      case 'ataque':
+        return 'assets/image/ataque_type.png';
+      case 'defensa':
+        return 'assets/image/defensa_type.png';
+      default:
+        return 'assets/image/ataque_type.png'; // Imagen por defecto si no se encuentra el tipo
+    }
+  }
+
+  IconData getIconForType(String type) {
+    switch (type) {
+      case 'ataque':
+        return Icons.sports_soccer; // Ícono de balón de fútbol
+      case 'defensa':
+        return Icons.shield_rounded; // Ícono de candado
+      default:
+        return Icons.help; // Ícono por defecto si no se encuentra el tipo
+    }
+  }
+
+  Future<void> _viewVideo(String videoUrl) async {
+
+  }
+
+  Future<void> _deleteVideo(String documentId, String videoUrl) async {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('football_plays').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No hay videos guardados'));
+        }
+
+        final videos = snapshot.data!.docs;
+
+        double screenWidth = MediaQuery.of(context).size.width;
+
+        // Calcula cuántas tarjetas se pueden mostrar en una fila
+        int crossAxisCount;
+        if (screenWidth < 600) {
+          crossAxisCount = 2; // Móviles
+        }  else {
+          crossAxisCount = 3; // Pantallas más grandes
+        }
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Jugadas Guardadas', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: crossAxisCount, // Usar el número calculado de tarjetas por fila
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 1.2,
+                ),
+                itemCount: videos.length,
+                itemBuilder: (context, index) {
+                  final plays = videos[index];
+                  final nombre = plays['name'];
+                  final tipo = plays['type'];
+                  final videoUrl = plays['videoUrl']; // Asumiendo que el URL del video está en el campo 'videoUrl'
+                  final documentId = plays.id; // El ID del documento para eliminar
+
+                  return Card(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(getBackgroundImage(tipo)), // Se utiliza el tipo de la jugada
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(getIconForType(tipo), size: 20, color: Colors.black),
+                                const SizedBox(width: 8.0),
+                                Text('$nombre', style: const TextStyle(fontSize: 14, color: Colors.black)),
+                              ],
+                            ),
+                            const SizedBox(height: 4.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.play_arrow),
+                                  onPressed: () => _viewVideo(videoUrl),
+                                  tooltip: 'Ver Video',
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () {
+                                    // Aquí puedes implementar la lógica para modificar el video
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(content: Text('Modificar video no implementado')),
+                                    );
+                                  },
+                                  tooltip: 'Modificar Video',
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () => _deleteVideo(documentId, videoUrl),
+                                  tooltip: 'Eliminar Video',
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
