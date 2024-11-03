b61a2531bf2d58a871c428b721a20969ab720a61
Gonzalo Marquez <gmdetorres@gmail.com>
2024-11-03 20:10:45 +0100

    💡 comments: add comments to the source code.

--- lib/provider/match_provider.dart
+++ lib/provider/match_provider.dart
@@ -1,10 +1,18 @@
+/*
+ * Archivo: match_provider.dart
+ * Descripción: Este archivo contiene la definición de la clase MatchProvider 
+ *              que permite conocer el partido que estamos visualizando.
+ * 
+ * Autor: Gonzalo Márquez de Torres
+ * 
+ */
 import 'package:flutter/material.dart';
 
 class MatchProvider with ChangeNotifier {
   String _selectedMatchId = ''; 
-
   String get selectedMatchId => _selectedMatchId;
 
+  /// Función que permite seleccionar el partido del que queremos visualizar los datos relativos a este.
   void setSelectedMatchId(String matchId) { 
     _selectedMatchId = matchId;
     notifyListeners(); 
--- lib/provider/team_provider.dart
+++ lib/provider/team_provider.dart
@@ -1,10 +1,18 @@
+/*
+ * Archivo: match_provider.dart
+ * Descripción: Este archivo contiene la definición de la clase TeamNameProvider 
+ *              que permite conocer los datos del equipo que estamos visualizando.
+ * 
+ * Autor: Gonzalo Márquez de Torres
+ * 
+ */
 import 'package:flutter/material.dart';
 
 class TeamProvider with ChangeNotifier {
   String _selectedTeamName = '';
-
   String get selectedTeamName => _selectedTeamName;
 
+  /// Función que permite seleccionar el equipo del que queremos visualizar los datos relativos a este.
   void setSelectedTeamName(String teamName) {
     _selectedTeamName = teamName;
     notifyListeners(); 
