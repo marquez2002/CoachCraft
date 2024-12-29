40afbecf0ae2775b36cd178540a9e2b6dff76003
Gonzalo Marquez <gmdetorres@gmail.com>
2024-12-29 20:08:37 +0100

    🐛 fix: Fixed android folder for app build.

--- android/app/src/main/kotlin/com/example/coachcraft/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coachcraft/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.coachcraft
 
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
     }
 }
